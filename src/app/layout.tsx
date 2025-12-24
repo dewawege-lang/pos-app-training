3d77d010fabba26d23f502743d28a56de39906ed
Dewa <dewa.wege@gmail.com>
2025-12-24 11:21:29 +0700

    feat(sidebar):finish open close button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SideBar from "@/Components/SideBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+           <div className="flex text-primary bg-darkbg2">
+              {/* sidebar */}
+              <SideBar />
+              {/* content */}
+              <div className="sm:w-[60%] w-full h-screen bg-black">{children}</div>
+              {/* sheet */}
+              <div className="sm:w-[300px] h-screen bg-slate-700"></div>
+            </div>
+        
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,9 @@
-import { IconBuilding, IconBuildingStore, IconBurger, IconDashboard, icons, IconSettingsCog } from "@tabler/icons-react";
-import Image from "next/image";
-import Link from "next/link";
-import { title } from "process";
+import SideBar from "@/Components/SideBar";
 
 export default function Home() {
-  const sideBarNavs = {
-    header:{
-      icon: <IconBuildingStore/>,
-      title: "Kasir App",
-      subTitle: "Kelola Penjualanmu",
-    },
-    navigations: [
-      {
-        icon: <IconDashboard/>,
-        title: "Dashboard",
-        url: "/dashboard"
-      },
-      {
-        icon: <IconBurger/>,
-        title: "Menu",
-        url: "/menu",
-      },
-      {
-        icon: <IconSettingsCog/>,
-        title: "Settings",
-        url: "/settings",
-      },      
-    ],
-    footer:{}
-  }
+
   return (
-    <div className="flex">
-      {/* sidebar */}
-      <div className="sm:w-[280] h-screen bg-slate-800 p-4">
-        {/* header */}
-        <div className="centered gap-3 font-semibold text-2xl bg-indigo-700 py-2 rounded-2xl">
-          <div className="*:size-10">{sideBarNavs.header.icon}</div>
-          <div>{sideBarNavs.header.title}</div>
-        </div>
-        {/* nav */}
-        <div className="my-2 p-4 flex flex-col gap-y-5">
-          {sideBarNavs.navigations.map((nav,index)=>(
-            <Link key={index} href={nav.url} className="flex gap-x-2 p-1.5 bg-indigo-900 rounded-xl px-3 hover:font-bold hover:bg-indigo-900/70">
-              {nav.icon}
-              <span>{nav.title}</span>
-            </Link>
-          ))}
-        </div>
-        {/* footer */}
-        <div></div>
-      </div>
-      {/* content */}
-      <div className="sm:w-[60%] w-full h-screen"></div>
-      {/* sheet */}
-      <div className="sm:w-[300px] h-screen bg-slate-700"></div>
-    </div>
+    <div></div>
 
   );
 }
